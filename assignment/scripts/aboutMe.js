d7eb314b9c68b8482d163cc32447146fb9345f12
Paul LeRoy <pleroy33@gmail.com>
2023-11-02 13:51:02 -0500

    created a conditional for adventurous

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -46,6 +46,10 @@ console.log(allowedPets)
 //      if adventurous is true, set `result` to be "Adventures are great!", 
 //      if it's not true,  set `result` to be "How about we stay home?"
 //      Console log the value of `result`
+let result = ""
+if(adventurous = true) {result = 'Adventures are great!'}  // we've already set result above so no let
+else if(adventurous = false) {result = "How about we stay home?"};
+console.log(result);
 
 // 13 - Create a variable called `diceRoll` and set it to the value of "Try again later.".
 //      Create a compound conditional: 
@@ -61,7 +65,6 @@ console.log(allowedPets)
 //      set `petStatus` to the value of "I have enough pets" 
 //      if the value of `pets` is greater than the value of `allowedPets`.
 //      set `petStatus` to the value of "Oh no, I have too many pets!"
-
 
 // STRETCH GOALS:
 
