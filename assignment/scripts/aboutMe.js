e8458930cc452a6849972460fccc187009748a20
Paul LeRoy <pleroy33@gmail.com>
2023-11-02 14:49:00 -0500

    fixed syntax in conditionals

--- assignment/scripts/aboutMe.js
+++ assignment/scripts/aboutMe.js
@@ -46,9 +46,9 @@ console.log(allowedPets)
 //      if adventurous is true, set `result` to be "Adventures are great!", 
 //      if it's not true,  set `result` to be "How about we stay home?"
 //      Console log the value of `result`
-let result = ""
+let result = "";
 if(adventurous = true) {result = 'Adventures are great!'}  // we've already set result above so no let
-else if(adventurous = false) {result = "How about we stay home?"};
+else{result = "How about we stay home?"}
 console.log(result);
 
 // 13 - Create a variable called `diceRoll` and set it to the value of "Try again later.".
@@ -56,7 +56,10 @@ console.log(result);
 //      if luckyNumber is 2 and adventurous is true,
 //      set `diceRoll` to be "Roll the dice!"
 //      Console log the value of `diceRoll`
-
+let diceRoll = "";
+if (luckyNumber === 2 && adventurous === true) {diceRoll = "Roll the dice!";}
+else {diceRoll = "Try again later.";}
+console.log(diceRoll);
 // 14 - Create a variable called `petStatus`. 
 //      Write a conditional that covers the following:
 //      if the value of `pets` is less than the value of `allowedPets`,
@@ -65,7 +68,11 @@ console.log(result);
 //      set `petStatus` to the value of "I have enough pets" 
 //      if the value of `pets` is greater than the value of `allowedPets`.
 //      set `petStatus` to the value of "Oh no, I have too many pets!"
-
+petStatus = '';
+if (pets < allowedPets){petStatus = 'I can have more pets';}
+else if (pets = allowedPets){petStatus = 'I have enough pets';}
+else {petStatus = 'Oh no, I have too many pets!';}
+console.log(petStatus);
 // STRETCH GOALS:
 
 // 15 - Make a variable called `mostPets` and a conditional that
